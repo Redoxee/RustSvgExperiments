439d94b195804d3de317acfa28d1345438da0061
Anton Roy <antonmakesgames@gmail.com>
2022-07-26 23:33:11 +0200

    switching to the neww tile system for hex grid

--- src/main.rs
+++ src/main.rs
@@ -15,19 +15,10 @@ use crate::font::*;
 mod signature;
 use crate::signature::*;
 
-struct Shape {
-    points:Vec<Vec2>,
-}
-
-impl Shape {
-    fn draw_mesh(&self, mb: &mut MeshBuilder){
-        mb.polygon(graphics::DrawMode::Stroke(graphics::StrokeOptions::default().with_line_width(2_f32)), &self.points, graphics::Color::BLACK).unwrap();
-    }
-}
-
 struct TileInfo {
     position : Vec2,
     neighbors : Vec<usize>,
+    vertices : Vec<Vec2>,
 }
 
 impl TileInfo {
@@ -35,6 +26,7 @@ impl TileInfo {
         TileInfo {
             position,
             neighbors : Vec::new(),
+            vertices : Vec::new(),
         }
     }
 }
@@ -44,6 +36,10 @@ struct Grid {
 }
 
 impl Grid {
+    fn hex_grid_size(col : usize, row : usize, tile_scale : f32) -> Vec2{
+        Vec2::new(col as f32 * tile_scale * 2_f32 * std::f32::consts::FRAC_PI_6.cos(), row as f32 * tile_scale * 3_f32 * std::f32::consts::FRAC_PI_6.sin())   
+    }
+
     fn hex_grid(col : usize, row : usize, tile_scale : f32, base_position : Vec2) -> Grid {
         let mut grid = Grid {
             tiles: Vec::new()
@@ -52,11 +48,19 @@ impl Grid {
         let max_x = col - 1;
         let max_y = row - 1;
 
+        let o = 0_f32;
+        let i = 1_f32 * tile_scale;
+        let si = std::f32::consts::FRAC_PI_6.sin() * tile_scale;
+        let co = std::f32::consts::FRAC_PI_6.cos() * tile_scale;
+
+        let tile_width = co * 2_f32;
+        let tile_height = si * 3_f32;
+
         for y in 0..row {
             for x in 0..col {
-                let mut position = base_position + Vec2::new(x as f32 * tile_scale, y as f32 * tile_scale);
+                let mut position = base_position + Vec2::new(x as f32 * tile_width, y as f32 * tile_height);
                 if y % 2 == 1 {
-                    position.x = position.x + tile_scale / 2_f32;
+                    position.x = position.x + co;
                 }
 
                 let mut tile = TileInfo::new(position);
@@ -104,14 +108,17 @@ impl Grid {
                         }
                     }
                 }
-
+                
+                tile.vertices.clear();
+                tile.vertices.push(position + Vec2::new(o, i));
+                tile.vertices.push(position + Vec2::new(-co, si));
+                tile.vertices.push(position + Vec2::new(-co, -si));
+                tile.vertices.push(position + Vec2::new(o, -i));
+                tile.vertices.push(position + Vec2::new(co, -si));
+                tile.vertices.push(position + Vec2::new(co, si));
                 
                 grid.tiles.push(tile);
             }
-        }
-
-        for tile_index in 0..grid.tiles.len() {
-            println!("{} : {:?}", tile_index, grid.tiles[tile_index].neighbors);
         }
 
         return grid;
@@ -119,9 +126,9 @@ impl Grid {
 }
 
 struct Application {
-    size: Vec2,
-    shapes:Vec<Shape>,
+    grid: Grid,
     scale: f32,
+    size: Vec2,
     font : Font,
 
     instructions: Vec<Instruction>,
@@ -131,66 +138,41 @@ struct Application {
 }
 
 impl Application {
-    fn new(size: Vec2, scale: f32, font : Font) -> Application{
+    fn new(grid: Grid, scale: f32, size: Vec2, font : Font) -> Application{
         Application {
-            size,
-            shapes: Vec::new(),
+            grid,
             is_mouse_down: false,
             is_print_down: false,
             instructions: Vec::new(),
             scale,
             font,
+            size,
         }
     }
 
-    fn add_hexagone(&mut self, position : Vec2, scale : f32) {
-        let mut points = Vec::new();
-        let scale = scale * self.scale;
-        let o = 0_f32;
-        let i = 1_f32 * scale;
-        let si = std::f32::consts::FRAC_PI_6.sin() * scale;
-        let co = std::f32::consts::FRAC_PI_6.cos() * scale;
-        points.push(position + Vec2::new(o, i));
-        points.push(position + Vec2::new(-co, si));
-        points.push(position + Vec2::new(-co, -si));
-        points.push(position + Vec2::new(o, -i));
-        points.push(position + Vec2::new(co, -si));
-        points.push(position + Vec2::new(co, si));
-        self.shapes.push(Shape{points : points});
-    }
-
-    fn clear(&mut self) {
-        self.shapes.clear();
-    }
-
-    fn print_shapes(&self) {
-        if self.shapes.len() == 0 && self.instructions.len() == 0 {
-            return
-        }
-
+    fn print_to_svg(&self) {
         let mut drawn_points = std::collections::HashSet::new();
         let mut data = Data::new();
 
-        if self.shapes.len() > 0 {
-
-            let mut shapes_to_draw : Vec<usize> = (0..self.shapes.len()).collect();
+        {
+            let mut shapes_to_draw : Vec<usize> = (0..self.grid.tiles.len()).collect();
             let mut next_shape_index = (shapes_to_draw.len() - 1) / 2;
             let mut start_vertex = 0 as usize;
             let mut delta = 0;
             
-            data = data.move_to((self.shapes[0].points[0] / self.scale).from());
+            data = data.move_to((self.grid.tiles[0].vertices[0] / self.scale).from());
             let mut current_position = Vec2::default();
             while !shapes_to_draw.is_empty() {
                 let shape_index = shapes_to_draw[next_shape_index];
                 shapes_to_draw.remove(next_shape_index);
-                let shape = &self.shapes[shape_index];
-                let mut current_vertex = shape.points[start_vertex];
+                let tile = &self.grid.tiles[shape_index];
+                let mut current_vertex = tile.vertices[start_vertex];
                 if current_vertex != current_position {
                     data = data.move_to((current_vertex / self.scale).from());
                 }
 
-                for vert_index in 1..(shape.points.len() + 1) {
-                    let next_vertex = shape.points[(vert_index + start_vertex) % shape.points.len()];
+                for vert_index in 1..(tile.vertices.len() + 1) {
+                    let next_vertex = tile.vertices[(vert_index + start_vertex) % tile.vertices.len()];
                     let pair = OrderedPair::new(current_vertex, next_vertex);
                     let next_point = (next_vertex / self.scale).from();
                     if !drawn_points.contains(&pair) {
@@ -210,9 +192,9 @@ impl Application {
 
                 let mut found = false;
                 for other_index in 0..shapes_to_draw.len() {
-                    let other_shape = &self.shapes[shapes_to_draw[other_index]];
-                    for other_vertex_index in 0..other_shape.points.len() {
-                        let other_vertex = other_shape.points[other_vertex_index];
+                    let other_tile = &self.grid.tiles[shapes_to_draw[other_index]];
+                    for other_vertex_index in 0..other_tile.vertices.len() {
+                        let other_vertex = other_tile.vertices[other_vertex_index];
                         if other_vertex == current_vertex {
                             start_vertex = other_vertex_index;
                             next_shape_index = other_index;
@@ -228,14 +210,13 @@ impl Application {
                 
                 if !found {
                     next_shape_index = (shapes_to_draw.len() - 1) / 2;
-                    start_vertex = delta % self.shapes[shapes_to_draw[next_shape_index]].points.len();
+                    start_vertex = delta % self.grid.tiles[shapes_to_draw[next_shape_index]].vertices.len();
                     delta = delta + 1;
                 }
                 
                 current_position = current_vertex;
             }
         }
-
         let mut current_position = Vec2::new(0_f32, 0_f32);
         for instruction in &self.instructions {
             match instruction {
@@ -286,43 +267,11 @@ impl Application {
 
 impl Application
 {
-    fn hex_grid(&mut self) {
-        let hex_scale= 4_f32;
-        let grid_size = Vec2::new(4_f32, 4_f32);
-        let child_chance = 0.0;
-        let decay = 0.7;
-
-        let hex_half_width = std::f32::consts::FRAC_PI_6.cos() * hex_scale * self.scale;
-        let hex_width = hex_half_width * 2_f32;
-        let hex_height = std::f32::consts::FRAC_PI_6.sin() * hex_scale * 3_f32 * self.scale;
-        let total_size = grid_size * Vec2::new(hex_width,hex_height);
-        let base_position = self.size / 2_f32 * self.scale - total_size / 2_f32 + Vec2::new(hex_half_width * 0.5, hex_height * 0.25);
-        
-        let mut random = rand::thread_rng();
-
-        let mut index = 0;
-        for y in 0..(grid_size.y as usize) {
-            for x in 0..(grid_size.x as usize) {
-                let fx = x as f32 * hex_width + (y % 2) as f32 * hex_half_width;
-                let fy = y as f32 * hex_height;
-                let mut child_scale = 1_f32;
-                let mut is_child = true;
-                while is_child {
-                    is_child = random.gen_range(0_f32..1_f32) > (1_f32 - child_chance);
-                    self.add_hexagone(Vec2::new(fx, fy) + base_position, hex_scale * child_scale);
-                    child_scale = child_scale * decay;
-                }
-
-                self.font.print_in_instructions(format!("{}", index), Vec2::new(fx, fy) + base_position + Vec2::new(-7.5, 0.0), 15_f32, &mut self.instructions);
-                index = index + 1;
-            }
-        }
-    }
-
     fn fill_mesh_builder(instructions : &Vec<Instruction>, mesh_builder : &mut MeshBuilder)
     {
         let line_width = 2_f32;
         let mut vertices = Vec::new();
+
         for instruction in instructions {
             match instruction {
                 Instruction::LineTo(pos) => {
@@ -357,8 +306,7 @@ impl ggez::event::EventHandler<GameError> for Application {
         self.is_mouse_down = input::mouse::button_pressed(ctx, event::MouseButton::Left);
         if was_pressed != self.is_mouse_down {
             if self.is_mouse_down {
-                self.clear();
-                self.hex_grid();
+                
             }
         }
 
@@ -366,7 +314,7 @@ impl ggez::event::EventHandler<GameError> for Application {
         self.is_print_down = input::keyboard::is_key_pressed(ctx, input::keyboard::KeyCode::P);
         if was_down != self.is_print_down && self.is_print_down
         {
-            self.print_shapes();
+            self.print_to_svg();
         }
 
         Ok(())
@@ -379,9 +327,11 @@ impl ggez::event::EventHandler<GameError> for Application {
         
         Application::fill_mesh_builder(&self.instructions, mb);
 
-        if self.shapes.len() > 0 {
-            for shape in &self.shapes {
-                shape.draw_mesh(mb);
+        for tile in &self.grid.tiles {
+            mb.polygon(graphics::DrawMode::Stroke(graphics::StrokeOptions::default().with_line_width(2_f32)), &tile.vertices, graphics::Color::BLACK).unwrap();
+        
+            for neighbor in &tile.neighbors {
+                mb.line(&[tile.position, self.grid.tiles[*neighbor].position], 3_f32, graphics::Color::BLACK).unwrap();
             }
         }
 
@@ -404,18 +354,19 @@ fn main() {
     c.window_mode.height = height * scale;
 
     let font = Font::load("Medias/HersheySans1.svgfont");
-
-    let mut application = Application::new(Vec2::new(width,height), scale, font);
+    
+    let col = 40;
+    let row = 40;
+    let tile_scale = 8_f32;
+    let grid_size = Grid::hex_grid_size(col, row, tile_scale);
+    let grid = Grid::hex_grid(col, row, tile_scale, Vec2::new((width * scale - grid_size.x) / 2_f32, (height * scale - grid_size.y) / 2_f32));
+    let mut application = Application::new(grid, scale, Vec2::new(width, height), font);
     
     let signature = get_signature();
     let signature_height = 9.0_f32;
     let signature_width = application.font.get_width(signature, signature_height);
 
     application.font.print_in_instructions(get_signature(), Vec2::new(width * scale - signature_width- 3_f32, height* scale - 3_f32), signature_height, &mut application.instructions);
-
-    let grid = Grid::hex_grid(4, 4, 10_f32, Vec2::new(0_f32, 0_f32));
-
-    application.hex_grid();
 
     let (ctx, event_loop) = ContextBuilder::new("SVG Experiment", "AntonMakesGames")
     .default_conf(c)
